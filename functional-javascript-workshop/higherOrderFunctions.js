5f15f6a20a48c7d8bb78b5d5839b62efb9623240
José Ricardo <zevatron@zevatron.com.br>
2017-11-16 17:50:09 -0300

    Functional Javascript

---
+++ functional-javascript-workshop/higherOrderFunctions.js
@@ -0,0 +1,13 @@
+function repeat(operation, num) {
+      // SOLUTION GOES HERE
+      for (var i = 0; i < num; i++) {
+      	operation
+      }
+      //com recursividade
+      //  if (num <= 0) return
+      //	operation()
+      //	return repeat(operation, --num)
+    }
+
+    // Do not remove the line below
+    module.exports = repeat
